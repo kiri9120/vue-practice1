bf332bb8a6720023fc7ef05914a054d5b098a864
Masato Kiryu <kiri9120@gmail.com>
2019-10-10 18:08:26 +0900

    create files

--- my-second-vue-cli-project/src/views/Home.vue
+++ my-second-vue-cli-project/src/views/Home.vue
@@ -2,20 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <SampleFile/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import SampleFile from '@/components/SampleFile.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
-    SampleFile
+    HelloWorld
   }
 }
 </script>
