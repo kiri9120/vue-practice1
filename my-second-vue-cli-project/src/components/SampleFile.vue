4fbd32d0d5742c3bff1961273d294306d50234c3
Masato Kiryu <kiri9120@gmail.com>
2019-10-10 12:27:23 +0900

    make test file

---
+++ my-second-vue-cli-project/src/components/SampleFile.vue
@@ -0,0 +1,12 @@
+<template>
+   <h3>これはサンプルです</h3>
+</template>
+
+<script>
+</script>
+<style scoped lang="scss">
+   h3 {
+      text-align: left;
+      color: red;
+   }
+</style>
--- my-second-vue-cli-project/src/views/Home.vue
+++ my-second-vue-cli-project/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SampleFile/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import SampleFile from '@/components/SampleFile.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    SampleFile
   }
 }
 </script>
